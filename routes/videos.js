63a2478a1163e65e22de243c7b3fad915981f946
De-Hype <iamsocialhype@gmail.com>
2023-12-24 02:40:24 -0800

    removed encryption and set up streaming for getting video

--- routes/videos.js
+++ routes/videos.js
@@ -7,7 +7,6 @@ const bucket = require("../utils/firebase.config");
 const router = require("express").Router();
 require("dotenv").config();
 
-
 const key = Buffer.from(process.env.key, "hex");
 const iv = Buffer.from(process.env.iv, "hex");
 
@@ -15,12 +14,24 @@ router.post(
   "/post-video",
   upload.single("video"),
   catchAsync(async (req, res, next) => {
+    //This was to store encrypted file or video
+    // const file = req.file;
+    // const encryptedFile = encryptVideo(file.buffer, key, iv);
+    // const filename =
+    //   crypto.randomBytes(16).toString("hex") + path.extname(file.originalname);
+    // const fileUpload = bucket.file(filename);
+    // await fileUpload.save(encryptedFile);
+    //This is to store raw video after it has been encrypted on the frontend
     const file = req.file;
-    const encryptedFile = encryptVideo(file.buffer, key, iv);
     const filename =
       crypto.randomBytes(16).toString("hex") + path.extname(file.originalname);
     const fileUpload = bucket.file(filename);
-    await fileUpload.save(encryptedFile);
+    await fileUpload.save(file);
+
+    //Will receive video and its name from the frontend
+    //Will generate a unique Id for the video
+    //Will store the video as a collection in Firebase
+    //
     res.status(200).json({
       success: true,
       message: " Video succesfully uploaded",
@@ -46,10 +57,39 @@ router.get(
   "/fetch-video/:filename",
   catchAsync(async (req, res, next) => {
     const filename = req.params.filename;
-    res.setHeader("Content-Type", "video/*");
+    const range = req.headers.range || `bytes=0-499`;
+
+    // res.setHeader("Content-Type", "video/*");
+    //Getting Video From Firebase
     const file = bucket.file(filename);
-    const readStream = file.createReadStream();
-    readStream.pipe(res);
+    //Getting MetaData about the video file to determine its size
+    const fileStat = await file.getMetadata();
+    const fileSize = fileStat[0].size;
+    console.log(range);
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      //Calculating the chunk size
+      const chunkSize = end - start + 1;
+      //Creating a readable stream
+      const readStream = file.createReadStream({ start, end });
+      res.writeHead(206, {
+        "Content-Range": `bytes ${start} - ${end} / ${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunkSize,
+        "Content-Type": "video/*",
+      });
+      readStream.pipe(res);
+    } else {
+      const readStream = file.createReadStream();
+      res.writeHead(206, {
+        "Content-Length": fileSize,
+        "Content-Type": "video/*",
+      });
+      readStream.pipe(res);
+    }
   })
 );
 
